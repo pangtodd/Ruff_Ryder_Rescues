5f827177cd045a2eda58ec622808ca2cbf4bbe54
Todd Pangilinan <pang.todd@gmail.com>
2022-06-30 23:32:13 -0700

    establish test for pets_delete path, passes.

---
+++ spec/requests/delete_pets_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe "delete a pet route", :type => :request do
+  before do
+    @pet = Pet.create!(:name => "test_pet", :animal_type => "dog")
+    delete "/pets/#{@pet.id}"
+  end
+
+  it "deletes the quote" do
+    expect(response).to have_http_status(202)
+    expect(JSON.parse(response.body)["message"]).to eq("entry successfully deleted.")
+  end
+end
