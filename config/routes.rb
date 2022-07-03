c929a3988a65a57aafb88ced581c64cbce8906b3
Todd Pangilinan <pang.todd@gmail.com>
2022-07-03 00:30:42 -0700

    attempted to set up new route for search. Didn't work, WIP.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :pets
+
+  get '/pets/search', to: 'pets#search'
+  #couldn't get this route to work quite right.
 end
