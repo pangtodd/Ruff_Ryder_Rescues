3cfcc14cf3323c2abc37904da08a58a7b58b5c6e
Todd Pangilinan <pang.todd@gmail.com>
2022-09-01 19:26:10 -0700

    upate to accommodate Swagger

--- spec/requests/delete_pets_spec.rb
+++ spec/requests/delete_pets_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 describe "delete a pet route", :type => :request do
   before do
+    Pet.destroy_all
     @pet = Pet.create!(:name => "test_pet", :animal_type => "dog")
     delete "/pets/#{@pet.id}"
   end
